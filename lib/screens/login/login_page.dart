8373d399a22efbc2b60aca77dfba3b06befd9003
Mehedi Hasan <mhasan172036@mscse.uiu.ac.bd>
2021-07-07 14:51:01 +0600

    modified

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -13,6 +13,60 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
+  void _showcontent() {
+    showDialog(
+      context: context, barrierDismissible: false, // user must tap button!
+
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Container(
+              height: 70,
+              width: 50,
+              child: Image.asset('assets/images/img3.png')),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Text(
+                  'Fringer Print',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 15),
+                Text('Used finger print to unlock',
+                    textAlign: TextAlign.center),
+              ],
+            ),
+          ),
+          actions: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10,bottom: 5),
+                  child: Container(
+                    width: 244,
+                    height: 52,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: Color(0xffe6f4ff),
+                    ),
+                    child: FlatButton(
+                      child: Text('Cancel', textAlign: TextAlign.center,),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -66,7 +120,6 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
                 SizedBox(height: size.height * 0.03),
-
                 Container(
                   width: 384,
                   height: 420,
@@ -209,15 +262,8 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                       SizedBox(height: size.height * 0.01),
                       RaisedButton(
-                        onPressed: () {
-                          // _onFormSubmitted();
-                          // Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //         builder: (context) => HomePage(
-                          //               size: MediaQuery.of(context).size,
-                          //             )));
-                        },
+                        onPressed: _showcontent,
+                        color: Colors.blueAccent,
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(6)),
                         textColor: Colors.white,
@@ -271,7 +317,8 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                       SizedBox(height: size.height * 0.01),
                       Text(
-                        "Powered by : Real Time Solution Software", textAlign: TextAlign.center,
+                        "Powered by : Real Time Solution Software",
+                        textAlign: TextAlign.center,
                         style: TextStyle(
                           color: Color(0xff194a83),
                           fontSize: 12,
